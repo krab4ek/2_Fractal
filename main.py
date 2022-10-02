54ec81a0df057f8d4cd05da054aa5ff348a71ddf
Covali Andrei <krab4ek@gmail.com>
2022-10-02 19:29:02 +0300

    update finish

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import turtle
 
-width = 500
-height = 500
+width = 1280
+height = 720
 screen = turtle.Screen()
 screen.setup(width,height,0,0)
 
-t = turtle.Turtle()
+def draw_koch_segment(t,ln):
+    ln_part = 8
+    if ln > ln_part:
+        ln_part = ln//3
+        draw_koch_segment(t,ln_part)
+        t.left(60)
+        draw_koch_segment(t, ln_part)
+        t.right(120)
+        draw_koch_segment(t,ln_part)
+        t.left(60)
+        draw_koch_segment(t,ln_part)
+            
+    else:
+        t.fd(ln)
+        t.left(60)
+        t.fd(ln)
+        t.right(120)
+        t.fd(ln)
+        t.left(60)
+        t .fd(ln)
 
-t.fd(20)
-t.left(90)
-t.fd(20)
-t.right(120)
-t.fd(40)
+t = turtle.Turtle()
+t.speed(100)
+t.ht()
+draw_koch_segment(t, 1000)
 
 turtle.done()
