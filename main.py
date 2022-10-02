ea9053f4309ee1aaa0cd14143785cebb359a9748
Covali Andrei <krab4ek@gmail.com>
2022-10-02 15:33:31 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+
+width = 500
+height = 500
+screen = turtle.Screen()
+screen.setup(width,height,0,0)
+
+t = turtle.Turtle()
+
+t.fd(20)
+t.left(90)
+t.fd(20)
+t.right(120)
+t.fd(40)
+
+turtle.done()
